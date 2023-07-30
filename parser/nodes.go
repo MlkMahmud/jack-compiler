e4e59731dc3a5953ad1bc6fbcbad86ac0ee3e866
Malik Mahmud <almalikmahmud@gmail.com>
2023-07-30 17:04:21 +0100

    add AST nodes for all statement types

--- parser/nodes.go
+++ parser/nodes.go
@@ -1,26 +1,83 @@
 package parser
 
-type ClassDeclaration struct {
-	Identifier             string
-	ClassVarDeclarations   []ClassVarDeclaration
-	SubroutineDeclarations []SubroutineDeclaration
+import "fmt"
+
+type Class struct {
+	Name        string
+	Subroutines []SubroutineDecl
+	Vars        []VarDecl
 }
 
-type ClassVarDeclaration struct {
-	Identifier string
-	Kind       string
-	Type       string
+type SubroutineKind int
+
+const (
+	Constructor SubroutineKind = iota
+	Function
+	Method
+)
+
+func (s SubroutineKind) String() string {
+	return []string{"constructor", "function", "method"}[s]
 }
 
-type SubroutineDeclaration struct {
-	Identifier      string
-	Kind            string
-	Type            string
-	VarDeclarations []VarDeclaration
-	Statements      []any
+type VarKind int
+
+const (
+	Field VarKind = iota
+	Static
+	Var
+)
+
+func (v VarKind) String() string {
+	return []string{"field", "static", "var"}[v]
 }
 
-type VarDeclaration struct {
-	Identifier string
-	Type       string
+type Expr interface {
+	fmt.Stringer
+}
+
+type Stmt interface {
+	fmt.Stringer
+}
+
+type VarDecl struct {
+	Name string
+	Kind VarKind
+	Type string
+}
+
+type SubroutineDecl struct {
+	Name       string
+	Params     []Expr
+	Kind       SubroutineKind
+	ReturnType string
+	Statements []Stmt
+	Vars       []VarDecl
+}
+
+type DoStmt struct {
+	Arguments      []Expr
+	ObjectName     string
+	SubroutineName string
+}
+
+type IfStmt struct {
+	Condition Expr
+	ThenStmt  Stmt
+	ElseStmt  Stmt
+}
+
+type LetStmt struct {
+	Name        string
+	ArrayAccess Expr
+	Value       Expr
+}
+
+type ReturnStmt struct {
+	Value Expr
+}
+
+type WhileStmt struct {
+	Condition  Expr
+	Statements []Stmt
 }
