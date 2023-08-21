589e30ea1652e07931579d235a9e1b94a5e00e92
Malik Mahmud <almalikmahmud@gmail.com>
2023-08-21 19:19:53 +0100

    test: update Parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,29 +2,56 @@ package parser_test
 
 import (
 	"encoding/json"
-	"fmt"
+	"io"
+	"log"
+	"os"
 	"path"
+	"strings"
 	"testing"
 
 	. "github.com/MlkMahmud/jack-compiler/lexer"
 	. "github.com/MlkMahmud/jack-compiler/parser"
+	. "github.com/nsf/jsondiff"
 )
 
 const TEST_DATA_PATH = "../testdata"
 
-func TestParser(t *testing.T) {
-	lexer := NewLexer()
-	filePath := path.Join(TEST_DATA_PATH, "SquareGame.jack")
-	tokens := lexer.Tokenize(filePath)
+func readFileContent(filename string) []byte {
+	file, _ := os.Open(filename)
+	bytes, err := io.ReadAll(file)
 
-	parser := NewParser()
-	class := parser.Parse(tokens)
-
-	jsonData, err := json.Marshal(class)
 	if err != nil {
-		fmt.Println(err)
-		t.Fatal()
+		log.Fatal(err)
 	}
+	return bytes
+}
 
-	fmt.Print(string(jsonData))
+func TestParser(t *testing.T) {
+	files := []string{"Array", "Square", "SquareGame"}
+	lexer := NewLexer()
+	parser := NewParser()
+
+	for _, file := range files {
+		t.Run(file, func(t *testing.T) {
+			filePath := path.Join(TEST_DATA_PATH, strings.Join([]string{file, "jack"}, "."))
+			cmpFilePath := path.Join(TEST_DATA_PATH, "expected", strings.Join([]string{file, "json"}, "."))
+			
+			tokens := lexer.Tokenize(filePath)
+			class := parser.Parse(tokens)
+			
+			expected := readFileContent(cmpFilePath)
+
+			actual, err := json.Marshal(class)
+
+			if err != nil {
+				t.Fatal(err)
+			}
+			
+			difference, desc := Compare(expected, actual, &Options{ SkipMatches: true })
+
+			if difference != FullMatch {
+				t.Fatal(desc)
+			}
+		})
+	}
 }
